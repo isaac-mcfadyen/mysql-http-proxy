249d32370b50eb0d8820574abe6a66339080074e
Isaac McFadyen <6243993+mcfadyeni@users.noreply.github.com>
2022-03-13 18:08:56 -0400

    First commit.

---
+++ src/main.ts
@@ -0,0 +1,39 @@
+import polka from "polka";
+import mysql from "mysql";
+import util from "util";
+import { config } from "dotenv";
+import bodyParser from "body-parser";
+const { json } = bodyParser;
+
+// Load the dotenv config.
+config();
+
+console.log("[INFO] Connecting to DB...");
+const connection = mysql.createConnection(process.env.CONNECTION_STRING);
+const queryDb = util.promisify(connection.query).bind(connection);
+console.log("[INFO] Connected!");
+
+console.log("[INFO] Starting server...");
+polka()
+  .use(json())
+  .post("/query", async (req, res) => {
+    // Get the JSON encoded body.
+    const { query } = req.body;
+
+    // If the query is undefined, return an error.
+    if (query === undefined) {
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ error: "No query provided." }));
+      return;
+    }
+
+    // Run the query on the DB.
+    const response = await queryDb(query);
+
+    // Return the response.
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(response));
+  })
+  .listen(3001, () => {
+    console.log("[INFO] Server started on port 3001");
+  });
